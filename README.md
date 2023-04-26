6119c8e934d9dff6f306266889b86dd96331304c
Alexandre Xisto Dos Santos Junior <20212TPMI0215@cefetes.br>
2023-04-26 20:00:15 -0300

    second commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Proj-Intergrador
+Confeitaria - Carol Cakes
+
+Projeto focado em auxiliar no atendimento ao cliente!
+
+Integrantes: Carlos Eduardo Oliveira Alves e Alexandre Xisto
+
+Site estático
---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+function home() {
+    element = document.querySelector("#menu")
+    element.scrollIntoView();
+}
+
+function sobre() {
+    element = document.querySelector(".sr-only")
+    element.scrollIntoView();
+}
+
+function duvidasEContato() {
+    element = document.querySelector("#perguntas")
+    element.scrollIntoView();
+}
+
+function duvidasEContatoBanner() {
+    element = document.querySelector("#copy-area")
+    element.scrollIntoView();
+}
